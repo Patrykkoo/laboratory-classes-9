15f8aaa78c6c8257b45fbcd71c65a7391ebb4158
Patrykkoo <patryk.chojnacki02@gmail.com>
2025-06-03 13:49:06 +0200

    Edycja app.js, przystosowanie pod mongodb

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const bookRoutes = require("./routes/bookRoutes");
 const authorRoutes = require("./routes/authorRoutes");
 
 dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const app = express();
-// app.use();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/books", bookRoutes);
 app.use("/api/authors", authorRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("✅ Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+})
+.catch(err => {
+  console.error("❌ MongoDB connection error:", err);
 });
